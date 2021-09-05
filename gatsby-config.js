414fcf1e92f647660e8b6f5ef3816e459e056e92
Dylan O'Leary <doleary@fortyfournorth.ca>
2021-09-05 17:20:34 -0400

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,25 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "clock-app",
+    title: "Wild Clock",
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-sass",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/icon.png",
+        name: `Wild Clock`,
+        short_name: `Wild Clock`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#FFFF00`,
+        display: `standalone`,
       },
     },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: { precachePages: ["/"] }
+    }
   ],
 };
